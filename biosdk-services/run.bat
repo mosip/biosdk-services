46e808f176b584aaee3a4cff74660e7a67ea7b92
kameshsr <kameshsr1338@gmail.com>
2026-06-04 12:31:29 +0530

    #1830 corrected version
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- biosdk-services/run.bat
+++ biosdk-services/run.bat
@@ -1,1 +1,1 @@
-java -Dloader.path=mock-sdk-1.3.0-SNAPSHOT-jar-with-dependencies.jar -Dbiosdk_bioapi_impl=io.mosip.mock.sdk.impl.SampleSDKV2 -jar target/biosdk-services-1.3.0-SNAPSHOT.jar
+java -Dloader.path=mock-sdk-1.4.0-SNAPSHOT-jar-with-dependencies.jar -Dbiosdk_bioapi_impl=io.mosip.mock.sdk.impl.SampleSDKV2 -jar target/biosdk-services-1.4.0-SNAPSHOT.jar
